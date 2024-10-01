faceef089100e6189b876fc48b344e2d959d3fa9
andretini <andrealijaramosagostini@gmail.com>
2024-09-30 22:15:26 -0300

    Relacionamento N x N entre Album e Faixa

--- albumDjango/Album/admin.py
+++ albumDjango/Album/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
 from .models import Album
+from .models import Faixa
 # Register your models here.
 
 @admin.register(Album)
 class AlbumAdmin(admin.ModelAdmin):
     list_display = ('titulo', 'artista', 'data_lancamento')
+
+@admin.register(Faixa)
+class TrackAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'duracao')
--- albumDjango/Album/models.py
+++ albumDjango/Album/models.py
@@ -7,3 +7,12 @@ class Album(models.Model):
 
     def __str__(self):
         return self.titulo
+
+
+class Faixa(models.Model):
+    titulo = models.CharField(max_length=200)
+    duracao = models.DurationField()
+    albuns = models.ManyToManyField(Album, related_name='faixas')  # Relacionamento N x N com Album
+
+    def __str__(self):
+        return self.title
